d85c62bb7603545d237f8fde32616e7737b084dc
Oleg <yko-v@yandex.ru>
2021-06-22 18:25:23 +0300

    So-so result with 8 act 0..500 range

--- lio/ml/generate_ml_data.m
+++ lio/ml/generate_ml_data.m
@@ -1,23 +1,23 @@
-N_TRAIN = 500;
-N_VALID = 500;
+N_TRAIN = 5000;
+N_VALID = 5000;
 
 XTdata = zeros(41,41,1,N_TRAIN);
-YTdata = zeros(N_TRAIN, 2);
+YTdata = zeros(N_TRAIN, 8);
 for i=1:N_TRAIN
-    y = rand(2, 1);
+    y = rand(8, 1);
     YTdata(i,:) = y;
-    x = takeImage([0 0], [y(1)*500 0 y(2)*500 0 0 0 0 0 0]*1e-9, 'IRS_180' , 0, 0);
+    x = takeImage([0 0], [y(1)*500 y(2)*500 y(3)*500 y(4)*500 0 y(5)*500 y(6)*500 y(7)*500 y(8)*500]*1e-9, 'IRS_180' , 0, 0);
     XTdata(:,:,1,i) = x(128-20:128+20, 128-20:128+20)./0.12;
     disp(i)
 end
 
 
 XVdata = zeros(41,41,1,N_VALID);
-YVdata = zeros(N_VALID, 2);
+YVdata = zeros(N_VALID, 8);
 for i=1:N_VALID
-    y = rand(2, 1);
+    y = rand(8, 1);
     YVdata(i,:) = y;
-    x = takeImage([0 0], [y(1)*500 0 y(2)*500 0 0 0 0 0 0]*1e-9, 'IRS_180' , 0, 0);
+    x = takeImage([0 0], [y(1)*500 y(2)*500 y(3)*500 y(4)*500 0 y(5)*500 y(6)*500 y(7)*500 y(8)*500]*1e-9, 'IRS_180' , 0, 0);
     XVdata(:,:,1,i) = x(128-20:128+20, 128-20:128+20)./0.12;
     disp(i)
 end
@@ -45,7 +45,7 @@ options = trainingOptions('sgdm', ...
     'Verbose',false);
 %}
 
-options = trainingOptions('adam', ...
+options = trainingOptions('sgdm', ...
     'LearnRateSchedule','piecewise', ...
     'LearnRateDropFactor',0.2, ...
     'LearnRateDropPeriod',5, ...
@@ -56,7 +56,7 @@ options = trainingOptions('adam', ...
     'ValidationFrequency',50,...
     'Plots','training-progress');
 
-net = trainNetwork(XTdata,YTdata,layers_29,options);
+net = trainNetwork(XTdata,YTdata,layers_41,options);
 
 pd = predict(net, XVdata);
-scatter(pd(:,2), YVdata(:,2));
+scatter(pd(:,4), YVdata(:,4));
